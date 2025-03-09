2273172a8416e058ee67fe13bb296249dee57613
Khalid Stewart <stewartkhalid5@gmail.com>
2025-03-08 21:44:11 -0800

    landing page update

--- main.js
+++ main.js
@@ -14,3 +14,21 @@ function myFunction() {
     btn.innerHTML = "Play";
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const fadeInSections = document.querySelectorAll(".fade-in-section");
+
+  const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add("is-visible");
+              observer.unobserve(entry.target); // Stop observing after it becomes visible
+          }
+      });
+  }, { threshold: 0.2 });
+
+  fadeInSections.forEach(section => observer.observe(section));
+});
+
+
+
